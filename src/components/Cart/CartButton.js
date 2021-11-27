0c2779140cfbc64b712a079c885448f6133210e2
eminoz <eminoz@yahoo.com>
2021-11-27 17:14:16 +0300

    establisted redux and added reducers

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,10 +1,22 @@
-import classes from './CartButton.module.css';
+import { useDispatch, useSelector } from "react-redux";
+import { uiActions } from "../../store/ui-slice";
+import classes from "./CartButton.module.css";
 
 const CartButton = (props) => {
+  const dispatch = useDispatch();
+  const cartQuantity = useSelector((state) => state.cart.totalQuantity);
+  /* 
+  store/index.js içerisindeki reduser içerisindeki cart storenı seçer 
+  ve cartSlice sayfasının içerisindeki initialState içerindeki totalQuantity datasını 
+  çekmiş oluruz
+  */
+  const toggleCartHanler = () => {
+    dispatch(uiActions.toggle());
+  };
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleCartHanler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartQuantity} </span>
     </button>
   );
 };
